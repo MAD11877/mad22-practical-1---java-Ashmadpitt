5920ce279d9622e3f2c8a79395028215c4aa7ef3
Ahmad Syafiq <92966858+Ashmadpitt@users.noreply.github.com>
2022-04-20 19:51:54 +0800

    Update Question1.java
    Edit with new hello world

--- Question1.java
+++ Question1.java
@@ -2,6 +2,7 @@ public class Question1
 {
   public static void main(String[] args)
   {
+    System.out.println("Hello world!"); 
     /**
      * Print "Hello World!"
      * 
@@ -9,3 +10,4 @@ public class Question1
      */
   }
 }
+
