fe1c9fed7d2a7b1a61ab501b2c17af4c5a364c22
Ahmad Syafiq <92966858+Ashmadpitt@users.noreply.github.com>
2022-04-24 12:40:12 +0800

    Update Question2.java
    Edited out the input print

--- Question2.java
+++ Question2.java
@@ -10,12 +10,9 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
     Scanner in = new Scanner(System.in);
-    System.out.println("Input height in meters: ");
     double height = in.nextDouble();
-    System.out.println("Input weight in kilogram: ");
     double weight = in.nextDouble();
     double BMI = weight / (height * height);
-    System.out.println("The Body Mass Index (BMI) in kg/m2 is ");
     System.out.println(BMI);
   }
 }
